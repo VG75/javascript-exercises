d430173caed1bb1c81c3443b7fd5e4246af2f69f
Vageesha G <vageeshag75@gmail.com>
2025-01-13 16:29:44 +0000

    Completed ninth challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.split(' ').join('').toLowerCase();
+    console.log(str);
+    let newString = "";
+    for (let i = 0; i < str.length; ++i) {
+        let ch = str.charCodeAt(i);
+        if (
+               
+            (ch >= 97 && ch <= 122) || // a-z
+            (ch >= 48 && ch <= 57) // 0-9
+        ) {
+            newString += str[i];
+        }
+    }
+    console.log(newString);
+    let left = 0, right = newString.length - 1;
+    while (left <= right) {
+        if (newString[left] != newString[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
 };
+
+//palindromes('Racecar');
 
 // Do not edit below this line
 module.exports = palindromes;
