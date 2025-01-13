172cdbb86ea03f3e06bd4823e397c288b8e594fd
Vageesha G <vageeshag75@gmail.com>
2025-01-13 17:08:46 +0000

    Completed twelth challenge

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const d = new Date();
+    const year = d.getFullYear();
 
+    let maxAge = -1;
+    let oldestPerson = {};
+
+    people.forEach(person => {
+        const personAge = (person.yearOfDeath ?? year) - person.yearOfBirth; // Using nullish coalescing for clarity
+        if (personAge > maxAge) {
+            maxAge = personAge; // Update maxAge here
+            oldestPerson = person;
+        }
+    });
+
+    return oldestPerson;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
